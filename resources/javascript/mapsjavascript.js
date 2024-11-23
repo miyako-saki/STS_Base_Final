c5721e1dc85ffd62492e52bcfef07915f9a02b76
David Xander Wagan <david.wagan@ciit.edu.ph>
2024-11-23 21:24:35 +0800

    updated restaurant list
    gagawa ako ng isang branch baka kasi magulo ko to, i'll be testing out different ways for the filter

--- resources/javascript/mapsjavascript.js
+++ resources/javascript/mapsjavascript.js
@@ -5,15 +5,31 @@ let directionsRenderer;
 // Categorized food stalls
 const categorizedFoodStalls = {
   budget: [
-    { lat: 14.629391, lng: 121.043842, name: "A&A Cafe Food & Beverage", priceRange: "₱" },
-    { lat: 14.629756, lng: 121.041807, name: "Ava's Carenderia", priceRange: "₱" }
+    { lat: 14.62658832747117, lng: 121.03708157937747, name: "Eli's Kainan Lugaw", priceRange: "₱" },
+    { lat: 14.62947448192729, lng: 121.04386416664083, name: "A&A Cafe Food & Beverage", priceRange: "₱" },
+    { lat: 14.629552626557162, lng: 121.0409937876642, name: "Masaderia", priceRange: "₱" },
+    { lat: 14.62975679975855, lng: 121.04180774981167, name: "Ava's Carenderia", priceRange: "₱" },
+    { lat: 14.62904360637158, lng: 121.03865726478979, name: "Atty. Lugaw", priceRange: "₱" }
+    
   ],
   affordable: [
-    { lat: 14.628957, lng: 121.042816, name: "Food Stall 3", priceRange: "₱₱" },
-    { lat: 14.631457, lng: 121.043316, name: "Food Stall 4", priceRange: "₱₱" }
+    { lat: 14.630618, lng: 121.045587, name: "Chowking", priceRange: "₱₱" },
+    { lat: 14.628561277368682, lng: 121.03405615129736, name: "Mcdonald's Tomas Morato", priceRange: "₱₱" },
+    { lat: 14.63237515624982, lng: 121.04150738013324, name: "Famouse BBQ & Silog Station", priceRange: "₱₱" },
+    { lat: 14.629625404087946, lng: 121.04207628191672, name: "Seoulful Sweets", priceRange: "₱₱" },
+    { lat: 14.631167071388862, lng: 121.04612828013339, name: "Jollibee Kamias EDSA", priceRange: "₱₱" },
+    { lat: 14.630208462344818, lng: 121.04378888198417, name: "Enjoy Your Coffee - EYC", priceRange: "₱₱" },
+    { lat: 14.637260439551637, lng: 121.03647333840752, name: "Cafe Roo", priceRange: "₱₱" },
+    { lat: 14.63471567795594, lng: 121.03568891082023, name: "Cafe I'm Here", priceRange: "₱₱" },
+    { lat: 14.630797380925037, lng: 121.04496361082005, name: "Goca Tea and Cafe", priceRange: "₱₱" },
+    { lat: 14.633672277560846, lng: 121.04024777897007, name: "Kim's Ramyun", priceRange: "₱₱" },
+    { lat: 14.628359858532221, lng: 121.0415847587011, name: "RM Coffee & Tea", priceRange: "₱₱" }
   ],
   expensive: [
-    { lat: 14.632457, lng: 121.044816, name: "Food Stall 5", priceRange: "₱₱₱" }
+    { lat: 14.629643531116596, lng: 121.04094530237712, name: "Angus Tapa Centrale", priceRange: "₱₱₱" },
+    { lat: 14.631285381961117, lng: 121.04125255649042, name: "Deo Gracias", priceRange: "₱₱₱" },
+    { lat: 14.6306323345192, lng: 121.04117011416216, name: "Zipang", priceRange: "₱₱₱" },
+    { lat: 14.632746198595271, lng: 121.04125628013328, name: "Victorino's Restaurant", priceRange: "₱₱₱" },
   ]
 };
 
